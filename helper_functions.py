8d29d3c4e9cfd6dfcd7c330834cf63fd82c55ab8
dr-philo <philipmarfo60@gmail.com>
2024-12-05 13:47:22 +0000

    Update helper_functions.py

--- helper_functions.py
+++ helper_functions.py
@@ -1,7 +1,7 @@
 # helper_functions.py
 import numpy as np
 import io
-
+from scipy.spatial.distance import pdist, squareform
 
 def read_xyz(file):
     content = file.getvalue().decode("utf-8").splitlines()
@@ -86,3 +86,36 @@ def delete_atoms(atomic_symbols, atomic_coordinates, atom_indices):
         del atomic_symbols[index]
         atomic_coordinates = np.delete(atomic_coordinates, index, axis=0)
     return atomic_symbols, atomic_coordinates
+
+def infer_bonds(atomic_symbols, atomic_coordinates, bond_threshold=1.6):
+    """
+    Infers bonds between atoms based on distances.
+    Returns a list of tuples representing bonds and their inferred order.
+    """
+    bond_list = []
+    bond_orders = []
+    
+    # Distance matrix for atomic coordinates
+    distances = squareform(pdist(atomic_coordinates))
+    
+    # Covalent radii approximation (Angstroms, sourced from standard values)
+    covalent_radii = {
+        "H": 0.31, "C": 0.76, "O": 0.66, "N": 0.71, "F": 0.57,
+        "Cl": 0.99, "Br": 1.14, "I": 1.33, "S": 1.02, "P": 1.07,
+    }
+    
+    # Iterate over atom pairs to identify bonds
+    for i in range(len(atomic_coordinates)):
+        for j in range(i + 1, len(atomic_coordinates)):
+            expected_bond_length = covalent_radii.get(atomic_symbols[i], 0.5) + covalent_radii.get(
+                atomic_symbols[j], 0.5)
+            margin = 0.2  # Adjust margin depending on desired accuracy
+            if distances[i, j] <= expected_bond_length + margin:
+                bond_list.append((i, j))
+                # Infer bond order based on bond length (simplified logic)
+                if distances[i, j] < expected_bond_length * 0.9:
+                    bond_orders.append(2)  # Double bond
+                else:
+                    bond_orders.append(1)  # Single bond
+                
+    return bond_list, bond_orders
